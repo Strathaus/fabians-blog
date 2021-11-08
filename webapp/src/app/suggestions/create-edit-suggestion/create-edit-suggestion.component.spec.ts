7b6215caa686805e89c714bc3d0141f100b1e5df
Fabian Strathaus <fabian.strathaus@gmail.com>
2021-11-08 09:45:52 +0100

    fixed frontend tests

--- webapp/src/app/suggestions/create-edit-suggestion/create-edit-suggestion.component.spec.ts
+++ webapp/src/app/suggestions/create-edit-suggestion/create-edit-suggestion.component.spec.ts
@@ -1,6 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from '../../lib/material.module';
 
 import { CreateEditSuggestionComponent } from './create-edit-suggestion.component';
 
@@ -11,7 +14,14 @@ describe('CreateEditSuggestionComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CreateEditSuggestionComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+      ],
     }).compileComponents();
   });
 
